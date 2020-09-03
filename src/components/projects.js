7f8daa70c7e4713c050374f8c8dd6deafacf7542
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-09-03 05:37:49 -0400

    added cards to all tabs

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,78 +1,371 @@
-import React, { Component } from 'react';
-import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions, Button, CardMenu, IconButton, CardText } from 'react-mdl'
+import React, { Component } from "react";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton,
+  CardText,
+} from "react-mdl";
 
 class Projects extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { activetab: 0 }
+  constructor(props) {
+    super(props);
+    this.state = { activetab: 0 };
+  }
+
+  // Depending on the tab will show corresponding data
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <div className="projects-grid">
+
+          {/* Project 1 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://reactjs.org/logo-og.png) center / cover",
+              }}
+            >
+              Employee Directory
+            </CardTitle>
+            <CardText>
+                An employee directory created in React that has search and sort functionality
+            </CardText>
+            <CardActions border>
+              <Button colored href='https://github.com/jyk524/Employee-Directory'>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored href='https://employee-directory-jyk.herokuapp.com/'>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 2 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://reactjs.org/logo-og.png) center / cover",
+              }}
+            >
+              {/* Project # */}
+            </CardTitle>
+            <CardText>
+              Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 3 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://reactjs.org/logo-og.png) center / cover",
+              }}
+            >
+              {/* Project # */}
+            </CardTitle>
+            <CardText>
+              Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div className="projects-grid">
+
+          {/* Project 1 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://reactjs.org/logo-og.png) center / cover",
+              }}
+            >
+              Employee Directory
+            </CardTitle>
+            <CardText>
+                An employee directory created in React that has search and sort functionality
+            </CardText>
+            <CardActions border>
+              <Button colored href='https://github.com/jyk524/Employee-Directory'>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored href='https://employee-directory-jyk.herokuapp.com/'>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 2 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://reactjs.org/logo-og.png) center / cover",
+              }}
+            >
+              {/* Project # */}
+            </CardTitle>
+            <CardText>
+              Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 3 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://reactjs.org/logo-og.png) center / cover",
+              }}
+            >
+              {/* Project # */}
+            </CardTitle>
+            <CardText>
+              Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div className="projects-grid">
+
+          {/* Project 1 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://reactjs.org/logo-og.png) center / cover",
+              }}
+            >
+              Employee Directory
+            </CardTitle>
+            <CardText>
+                An employee directory created in React that has search and sort functionality
+            </CardText>
+            <CardActions border>
+              <Button colored href='https://github.com/jyk524/Employee-Directory'>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored href='https://employee-directory-jyk.herokuapp.com/'>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 2 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://reactjs.org/logo-og.png) center / cover",
+              }}
+            >
+              {/* Project # */}
+            </CardTitle>
+            <CardText>
+              Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 3 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://reactjs.org/logo-og.png) center / cover",
+              }}
+            >
+              {/* Project # */}
+            </CardTitle>
+            <CardText>
+              Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      );
+    } else if (this.state.activeTab === 3) {
+      return (
+        <div className="projects-grid">
+
+          {/* Project 1 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://reactjs.org/logo-og.png) center / cover",
+              }}
+            >
+              Employee Directory
+            </CardTitle>
+            <CardText>
+                An employee directory created in React that has search and sort functionality
+            </CardText>
+            <CardActions border>
+              <Button colored href='https://github.com/jyk524/Employee-Directory'>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored href='https://employee-directory-jyk.herokuapp.com/'>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 2 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://reactjs.org/logo-og.png) center / cover",
+              }}
+            >
+              {/* Project # */}
+            </CardTitle>
+            <CardText>
+              Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 3 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://reactjs.org/logo-og.png) center / cover",
+              }}
+            >
+              {/* Project # */}
+            </CardTitle>
+            <CardText>
+              Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      );
     }
+  }
 
-// Depending on the tab will show corresponding data
-    toggleCategories() {
-        if(this.state.activeTab === 0) {
-        return(
-           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-               <CardTitle style={{color:'#fff', height: '176px', background: 'url(https://reactjs.org/logo-og.png) center / cover'}}>React Project #1</CardTitle>
-               <CardText>
-                   Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem 
-               </CardText>
-               <CardActions border>
-                <Button colored>Github</Button>
-                <Button colored>CodePen</Button>
-                <Button colored>LiveDemo</Button>
-               </CardActions>
-               <CardMenu style={{color: '#fff'}}>
-                <IconButton name="share"/>
-               </CardMenu>
-           </Card>
-            )
-        } else if(this.state.activeTab === 1){
-            return(
-                <div>
-                    <h1>This is NodeJS</h1>
-                </div>
-                )
-        } else if(this.state.activeTab === 2){
-            return(
-                <div>
-                    <h1>This is MongoDB</h1>
-                </div>
-                )
-        } else if(this.state.activeTab === 3){
-            return(
-                <div>
-                    <h1>This is JavaScript</h1>
-                </div>
-                )
-        }
+  render() {
+    return (
+      <div className="category-tabs">
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={(tabId) => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>NodeJS</Tab>
+          <Tab>MongoDB</Tab>
+          <Tab>JavaScript</Tab>
+        </Tabs>
 
-    }
-    
-
-    render() {
-        return (
-            <div className="category-tabs">
-                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
-                    <Tab>React</Tab>
-                    <Tab>NodeJS</Tab>
-                    <Tab>MongoDB</Tab>
-                    <Tab>JavaScript</Tab>
-                </Tabs>
-
-                <section className="projects-grid">
-                    <Grid className="projects-grid">
-                        <Cell col={12}>
-                            <div className="content">
-                                {this.toggleCategories()}
-                            </div>
-                        </Cell>
-                    </Grid>
-                    
-                </section>
-                
-            </div>
-        )
-    }
+          <Grid>
+            <Cell col={12}>
+              <div className="content">{this.toggleCategories()}</div>
+            </Cell>
+          </Grid>
+      </div>
+    );
+  }
 }
 
 export default Projects;
