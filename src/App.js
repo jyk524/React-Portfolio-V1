a6b484e28696a5587ce3b5c2c0f061d32c4c95c5
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-09-03 00:59:15 -0400

    added file structure and working react router

--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Button, Card, CardText } from 'react-mdl';
+import Main from './components/main'
 
-
-function App() {
-  return (
-  //  Uses a header that scrolls with the text, rather than staying locked at the top 
-<div class="mdl-layout mdl-js-layout">
-  <header class="mdl-layout__header mdl-layout__header--scroll">
-    <div class="mdl-layout__header-row">
-      {/* Title */}
-      <span class="mdl-layout-title">Title</span>
-      {/*  Add spacer, to align navigation to the right  */}
-      <div class="mdl-layout-spacer"></div>
-      {/*  Navigation  */}
-      <nav class="mdl-navigation">
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-      </nav>
-    </div>
-  </header>
-  <div class="mdl-layout__drawer">
-    <span class="mdl-layout-title">Title</span>
-    <nav class="mdl-navigation">
-      <a class="mdl-navigation__link" href="">Link</a>
-      <a class="mdl-navigation__link" href="">Link</a>
-      <a class="mdl-navigation__link" href="">Link</a>
-      <a class="mdl-navigation__link" href="">Link</a>
-    </nav>
-  </div>
-  <main class="mdl-layout__content">
-    <div class="page-content">
-      {/* <!-- Your content goes here --> */}
-    </div>
-  </main>
-</div>
-  );
+class App extends Component {
+  render () {
+    return (
+      // Scrolling Navbar
+      <div class="mdl-layout mdl-js-layout">
+        <header class="mdl-layout__header mdl-layout__header--scroll">
+          <div class="mdl-layout__header-row">
+            {/* <!-- Title --> */}
+            <span class="mdl-layout-title">James Y. Kim</span>
+            {/* <!-- Add spacer, to align navigation to the right --> */}
+            <div class="mdl-layout-spacer"></div>
+            {/* <!-- Navigation --> */}
+            <nav class="mdl-navigation">
+              <a class="mdl-navigation__link" href="/resume">Resume</a>
+              <a class="mdl-navigation__link" href="/aboutme">About Me</a>
+              <a class="mdl-navigation__link" href="/projects">Projects</a>
+              <a class="mdl-navigation__link" href="/contact">Contact</a>
+            </nav>
+          </div>
+        </header>
+        <div class="mdl-layout__drawer">
+          <span class="mdl-layout-title">James Y. Kim</span>
+          <nav class="mdl-navigation">
+            <a class="mdl-navigation__link" href="/resume">Resume</a>
+            <a class="mdl-navigation__link" href="/aboutme">About Me</a>
+            <a class="mdl-navigation__link" href="/projects">Projects</a>
+            <a class="mdl-navigation__link" href="/contact">Contact</a>
+          </nav>
+        </div>
+        <main class="mdl-layout__content">
+          <div class="page-content">
+            <Main />
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
+
 
 export default App;
